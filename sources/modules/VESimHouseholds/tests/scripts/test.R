56c0655e5bfe363a04a8c8968f994e9570e7a0c9
gregorbj <gregorbj@gmail.com>
2018-10-29 08:51:59 -0700

    Revise test script to save test results in central directory

--- sources/modules/VESimHouseholds/tests/scripts/test.R
+++ sources/modules/VESimHouseholds/tests/scripts/test.R
@@ -40,6 +40,15 @@ testModule(
 #Finish up
 setwd("tests")
 tar("Datastore.tar", "Datastore")
+file.copy("Datastore.tar", "../../Test_Data/VE-State/Datastore/Datastore.tar")
+file.copy("ModelState.Rda", "../../Test_Data/VE-State/ModelState/ModelState.Rda")
+file.copy("Log_CreateHouseholds.txt", "../../Test_Data/VE-State/Log/Log_CreateHouseholds.txt")
+file.copy("Log_PredictWorkers.txt", "../../Test_Data/VE-State/Log/Log_PredictWorkers.txt")
+file.copy("Log_AssignLifeCycle.txt", "../../Test_Data/VE-State/Log/Log_AssignLifeCycle.txt")
+file.copy("Log_PredictIncome.txt", "../../Test_Data/VE-State/Log/Log_PredictIncome.txt")
 dir.remove("Datastore")
+file.remove(c("Datastore.tar", "ModelState.Rda", "Log_CreateHouseholds.txt",
+              "Log_PredictWorkers.txt", "Log_AssignLifeCycle.txt",
+              "Log_PredictIncome.txt"))
 setwd("..")
 
